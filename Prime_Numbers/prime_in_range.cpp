889e78a4e12f80ffbe25ff64962b3cfd32097da9
spacewafer <adimohan29@gmail.com>
2018-12-18 14:17:54 +0530

    Prime Number issue
    x

--- Prime_Numbers/prime_in_range.cpp
+++ Prime_Numbers/prime_in_range.cpp
@@ -7,15 +7,20 @@ using namespace std;
 int main()
 {
     int range;
-    cout<<"enter the maximum range of prime number"; // 1<prime<range
+    cout<<"enter the maximum range of prime number \n"; // 1<prime<range
     cin>>range;
-    cout<<"Prime numbers between 1 to "<<range;
+    cout<<"Prime numbers between 1 to "<<range<<"\n";
     prime_fun(range,2);
     return 0;
 }
 
 void prime_fun(int range,int number)
 {
+   if(range == 1 || range == 2)					// 1 is not a prime number.
+	{cout<<"No Prime Numbers. Exiting Program. ";
+	return;
+	}
+
     if(number==range)
         return;
 
